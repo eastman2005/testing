143ee04bd2b2274e2aaa3a7408f4f5bf404818ef
Виктор <eastman2005@ya.ru>
2015-08-16 22:51:22 +0300

    Update Ideal_Weight.rb
    Ошибка типов...

--- Ideal_Weight.rb
+++ Ideal_Weight.rb
@@ -2,10 +2,10 @@ Puts "Введите свое имя"
 client = gets.chomp
 
 Puts "Введите свой рост"
-Hight = gets.chomp
+Hight = gets.chomp # не понял, что такое "chomp"
 
-if Hght - 110 >0 then
-  puts client ", ваш рост " Hght - 110
+if Hght.to_i - 110 >0 then
+  puts client ", ваш рост " Hght.to_i - 110
 Else
   puts "Ваш вес уже оптимальный"
 End
