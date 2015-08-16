677bbe48204bb190ca6dbf951d4d99b3ae6e589d
Виктор <eastman2005@ya.ru>
2015-08-16 03:05:00 +0300

    Create Ideal_Weight.rb
    1 part.1

---
+++ Ideal_Weight.rb
@@ -0,0 +1,10 @@
+Puts "Введите свое имя"
+client = gets.chomp
+
+Puts "Введите свой рост"
+Hight = gets.chomp
+
+if Hght - 110 >0 then
+  puts client ", ваш рост " Hght - 110
+Else
+  puts "Ваш вес уже оптимальный"
