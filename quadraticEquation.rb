3ab42f396e1f072d88feb4e2eb70c43de873dfb0
Виктор <eastman2005@ya.ru>
2015-08-20 04:07:49 +0300

    добавил пробелы и применил интерполяцию
    добавил пробелы для выделения условий и применил интерполяцию  при выводе переменных в строках

--- quadraticEquation.rb
+++ quadraticEquation.rb
@@ -1,22 +1,22 @@
 puts "Введите 1й коэффициент"
-a = gets.chomp.to_i
+a = gets.chomp.to_f
 puts "Введите 2й коэффициент"
-b = gets.chomp.to_i
+b = gets.chomp.to_f
 puts "Введите 3й коэффициент"
-c = gets.chomp.to_i
+c = gets.chomp.to_f
 
 # Вычисление дискриминанта
 dis = b**2 - 4*a*c
 puts "Дискриминант квадратного уравнения равен " + dis.to_s
 # Нахождение корней квадратного уравнения
 if dis < 0 
-puts "Корней нет"
+  puts "Корней нет"
 elsif dis > 0
-x1 = (-b + Math.sqrt(dis))/2 * a
-x2 = (-b - Math.sqrt(dis))/2 * a
+  x1 = (-b + Math.sqrt(dis))/2 * a
+  x2 = (-b - Math.sqrt(dis))/2 * a
 elsif dis == 0
-x1 = -b/(2*a)
-x2 = x1
+  x1 = -b/(2*a)
+  x2 = x1
 end
 
-puts "Корни квадратного уравнения: " + x1.to_s + " и " + x2.to_s
+puts "Корни квадратного уравнения: #{x1} и #{x2}"
